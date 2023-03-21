769472a3dd8666764b92e48f30339854ce15735c
hellojhonas <jhonacenas0704@gmail.com>
2023-03-21 22:05:52 +0800

    modifying loader

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,24 +1,8 @@
-var loader;
-
-function loadNow(opacity) {
-  if (opacity <= 0) {
-    displayContent();
-  } else {
-    loader.style.opacity = opacity;
-    window.setTimeout(function() {
-      loadNow(opacity - 0.05);
-    }, 50);
-  }
-}
-
-function displayContent() {
-  loader.style.display = 'none';
-  document.getElementById('content').style.display = 'block';
-}
-
 document.addEventListener("DOMContentLoaded", function() {
-  loader = document.getElementById('loader');
-  window.setTimeout(function() {
-    loadNow(1);
-  }, 1500); // wait 2 seconds before calling loadNow(1)
-});
+    var loader = document.getElementById('loader');
+    var content = document.getElementById('content');
+    setTimeout(function() {
+      loader.style.display = 'none';
+      content.style.display = 'block';
+    }, 2000); // wait 2 seconds before displaying content
+  });
